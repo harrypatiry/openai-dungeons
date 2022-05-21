090f5a321bfa7418439e72a313dc9602266f412f
harrypatiry <harrypatiry@gmail.com>
2022-05-21 12:47:21 -0400

    ADA

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Html from "next/document";
 import { useState } from "react";
 import styles from "./index.module.css";
 
@@ -30,33 +31,35 @@ export default function App() {
   }
 
   return (
-    <div>
-      <Head>
-        <title>OpenAI RPG</title>
-      </Head>
+    <Html lang="en">
+      <div>
+        <Head>
+          <title>OpenAI RPG</title>
+        </Head>
 
-      <main className={styles.main}>
-        <h3>Dungeons and Dragons Generator</h3>
-        <label for="Name"></label>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="nameInput"
-            placeholder="Enter your name"
-            value={nameInput}
-            onChange={(e) => setNameInput(e.target.value)}
-          />
-          <input type="submit" value="Generate Story" onClick={toggleList}/>
-        </form>
-        <h4>Stories</h4>
-        <div>
-          <ul className={styles.listContainer} style={{display: showList ? 'block' : 'none' }}>
-            {names.reverse().map((i, index) => (
-                <li className={styles.list} key={index}>Name: {i}<br></br>Story: {resultList[index]}</li>
-            ))}
-          </ul>
-        </div>
-      </main>
-    </div>
+        <main className={styles.main}>
+          <h3>Dungeons and Dragons Generator</h3>
+          <form onSubmit={onSubmit}>
+          <label for="Name">Name</label>
+            <input
+              type="text"
+              name="nameInput"
+              placeholder="Enter your name"
+              value={nameInput}
+              onChange={(e) => setNameInput(e.target.value)}
+            />
+            <input type="submit" value="Generate Story" onClick={toggleList}/>
+          </form>
+          <h4>Stories</h4>
+          <div>
+            <ul className={styles.listContainer} style={{display: showList ? 'block' : 'none' }}>
+              {names.reverse().map((i, index) => (
+                  <li className={styles.list} key={index}>Name: {i}<br></br>Story: {resultList[index]}</li>
+              ))}
+            </ul>
+          </div>
+        </main>
+      </div>
+    </Html>
   );
 }
